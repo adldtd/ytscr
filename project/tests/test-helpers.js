920f54e4c5ec1d6962ca3f3194cfd1dc1816f6cf
adldtd <alexlytvynjr@gmail.com>
2023-03-02 13:41:03 -0500

    Added testing to the video module

---
+++ project/tests/test-helpers.js
@@ -0,0 +1,288 @@
+const path = require("path");
+const cli = require(path.join(__dirname, "..", "entry", "cli")).cli;
+
+
+//*********************************************************************************
+//Mini shell parser for simpler looking testing; doesn't work with everything
+//(including escape characters)
+//*********************************************************************************
+function parse(shellArgs) {
+  shellArgs = shellArgs.trim();
+
+  let parsed = ["PROCESS_EXECPATH", "FILEPATH"]; //Dummy arguments
+  if (shellArgs.length === 0) return parsed;
+  let pack = null; //Whenever the next non-space character is found, pack is pushed into parsed
+  let inQuotes = false;
+  let quotesType = "\"";
+
+  let lastIndex = 0; //This index is always at the first found non-space character after a space
+
+  for (let i = 1; i < shellArgs.length; i++) {
+
+    if (inQuotes) { //Special processing mode; ignores spaces
+
+      if (shellArgs[i] === quotesType) {
+        inQuotes = false;
+        if (pack === null) pack = shellArgs.substring(lastIndex + 1, i);
+        else pack += shellArgs.substring(lastIndex + 1, i);
+
+        if (i < shellArgs.length + 1 && shellArgs[i + 1] !== " ")
+          lastIndex = i + 1;
+      }
+
+    } else if (shellArgs[i] !== " ") { //Normal processing mode
+      let justLanded = false; //If we "land" at a quote, then the text behind it is all spaces. Otherwise, we combine the previous text with the embedded quote text
+
+      if (shellArgs[i - 1] === " ") { //Push pack onto stack
+        parsed.push(pack);
+        pack = null;
+        lastIndex = i;
+        justLanded = true;
+      }
+
+      if (i < shellArgs.length + 1 && shellArgs[i + 1] === " ") //Store in pack whenever a seperator is found
+        pack = shellArgs.substring(lastIndex, i + 1);
+
+      if (shellArgs[i] === '"' || shellArgs[i] === "'") {
+        if (!justLanded) {
+          if (pack === null) pack = shellArgs.substring(lastIndex, i + 1);
+          else pack += shellArgs.substring(lastIndex, i + 1);
+          lastIndex = i;
+        }
+        quotesType = shellArgs[i];
+        inQuotes = true;
+      }
+    }
+  }
+
+  let i = shellArgs.length - 1;
+
+  if (inQuotes) {
+    if (pack === null) pack = shellArgs.substring(lastIndex + 1);
+    else pack += shellArgs.substring(lastIndex + 1);
+  } else if (shellArgs[i] !== '"' && shellArgs[i] !== "'") {
+    if (pack === null) pack = shellArgs.substring(lastIndex);
+    else pack += shellArgs.substring(lastIndex);
+  }
+
+  if (pack !== null) parsed.push(pack);
+  return parsed;
+}
+
+//*********************************************************************************
+//Mimics the function of the ytscr CLI, retrieving data using shell arguments
+//(without saving anything to disk.) Returns both settings and scraped data,
+//and expects valid (non --help) shell input
+//*********************************************************************************
+async function getData(shellArgs, onlySettings = false) {
+
+  let parsed = parse(shellArgs);
+  let module = parsed[2];
+  let pack = cli(parsed);
+
+  let scrape = pack[0];
+  let settings = pack[1];
+
+  settings[module].save = false;
+  return [(!onlySettings) ? await scrape(settings) : null, settings];
+}
+
+
+//*********************************************************************************
+//Checks if a module adheres with given options; should be called by larger test
+//functions
+//*********************************************************************************
+function diagStd(savedModule, ignore, limit) {
+
+  let stream = [];
+  if (Array.isArray(savedModule)) {
+    stream = savedModule;
+    if (stream.length > limit) //1st test
+      throw Error("Expected limit to be <= to " + limit + ", recieved " + stream.length);
+  } else //Non-stream like data (like the meta module)
+    stream.push(savedModule);
+
+  for (let item in stream) {
+    item = stream[item];
+
+    for (key in ignore) { //2nd test
+      if (key in item && ignore[key][0])
+        throw Error("Ignored attribute \"" + key + "\" found in item:\n" + JSON.stringify(item, null, 2));
+      if (!(key in item) && !ignore[key][0])
+        throw Error("Unignored attribute \"" + key + "\" could not be found in item:\n" + JSON.stringify(item, null, 2));
+    }
+  }
+}
+
+//*********************************************************************************
+//Count all comments, including replies to comments
+//*********************************************************************************
+function countComments(savedModule) {
+  let count = 0;
+  for (item in savedModule) {
+    item = savedModule[item];
+    if ("replies" in item) count += item.replies.length;
+    ++count;
+  }
+  return count;
+}
+
+//*********************************************************************************
+//Checks if the recommended module adheres with given options; called during video
+//testing
+//*********************************************************************************
+function diagComments(savedModule, ignore, limit, norp) {
+
+  let stream = savedModule;
+  if (limit != Number.POSITIVE_INFINITY && countComments(stream) > limit) //1st test
+    throw Error("Expected limit to be <= to " + limit + ", recieved " + stream.length);
+
+  for (let item in stream) {
+    item = stream[item];
+
+    for (key in ignore) { //2nd test
+      if (key in item && ignore[key][0])
+        throw Error("Ignored attribute \"" + key + "\" found in item:\n" + JSON.stringify(item, null, 2));
+      if (!(key in item) && !ignore[key][0])
+        throw Error("Unignored attribute \"" + key + "\" could not be found in item:\n" + JSON.stringify(item, null, 2));
+    }
+
+    if ("replies" in item && norp) //3rd test
+      throw Error("\"norp\" specified, but key \"replies\" found in item:\n" + JSON.stringify(item, null, 2));
+    if (!("replies" in item) && !norp)
+      throw Error("\"norp\" not specified, but key \"replies\" could not be found in item:\n" + JSON.stringify(item, null, 2));
+
+    if ("replies" in item)
+      diagComments(item.replies, ignore, Number.POSITIVE_INFINITY, true);
+  }
+}
+
+//*********************************************************************************
+//Checks if the recommended module (in "stream" form) adheres with given options;
+//called during video testing
+//*********************************************************************************
+function diagRecommended(savedModule, focus, ignore, limits, overallLimit) {
+
+  let stream = savedModule;
+  let limit = 0;
+  for (let lim in limits) {
+    if (focus[lim][0])
+      limit += limits[lim][0];
+  }
+  
+  if (stream.length > limit) //1st test
+    throw Error("Expected limit to be <= to " + limit + ", recieved " + stream.length);
+  if (stream.length > overallLimit)
+    throw Error("Expected limit to be <= to " + overallLimit + " (for recommended), recieved " + stream.length);
+
+  for (let item in stream) {
+    item = stream[item];
+
+    if (!focus[item.type][0]) //2nd test
+      throw Error("Item with type \"" + item.type + "\" recieved, yet module ignored:\n" + JSON.stringify(item, null, 2));
+
+    let moduleIgnore = ignore[item.type][1];
+    for (key in moduleIgnore) { //3rd test
+      if (key in item && moduleIgnore[key][0])
+        throw Error("Ignored attribute \"" + key + "\" found in item:\n" + JSON.stringify(item, null, 2));
+      if (!(key in item) && !moduleIgnore[key][0])
+        throw Error("Unignored attribute \"" + key + "\" could not be found in item:\n" + JSON.stringify(item, null, 2));
+    }
+  }
+}
+
+
+//*********************************************************************************
+//Checks if the submodules of the video modules adhere with given options
+//*********************************************************************************
+function testVideo(savedData, focus, ignore, limits, norp) {
+
+  for (let module in focus) { //1st test
+    if (module in savedData && !focus[module][0])
+      throw Error("Unfocused submodule \"" + module + "\" found in saved data:\n" + JSON.stringify(savedData, null, 2));
+    if (!(module in savedData) && focus[module][0])
+      throw Error("Focused submodule \"" + module + "\" could not be found in saved data:\n" + JSON.stringify(savedData, null, 2));
+
+    if (!(module in savedData)) continue;
+
+    if (module === "recommended") { //Special case
+
+      if (Array.isArray(savedData[module])) //Stream of data
+        diagRecommended(savedData[module], focus[module][1], ignore[module][1], limits[module][1]);
+      else {
+
+        let innerFocus = focus[module][1];
+        let innerData = savedData[module];
+
+        for (let innerModule in innerFocus) {
+          if (innerModule in innerData && !innerFocus[innerModule][0])
+            throw Error("Unfocused submodule \"" + innerModule + "\" found in saved data:\n" + JSON.stringify(innerData, null, 2));
+          if (!(innerModule in innerData) && innerFocus[innerModule][0])
+            throw Error("Focused submodule \"" + innerModule + "\" could not be found in saved data:\n" + JSON.stringify(innerData, null, 2));
+
+          if (!(innerModule in innerData)) continue;
+          diagStd(innerData[innerModule], ignore[module][1][innerModule][1], limits[module][1][innerModule][0], limits[module][0]);
+        }
+      }
+    
+    } else if (module === "comments") {
+      diagComments(savedData[module], ignore[module][1], limits[module][0], norp);
+    } else
+      diagStd(savedData[module], ignore[module][1], limits[module][0]);
+  }
+}
+
+
+//*********************************************************************************
+//Maps a specified value to modules, its submodules, and their attributes; makes
+//assumptions on the structure of "settings"
+//*********************************************************************************
+function mapVal(module, settings, val) {
+  let mappedSettings = {};
+  if (typeof(settings) !== "object") return mappedSettings; //Base case; "module" is actually an attribute
+
+  if ("focus" in settings) { //Assumes that the key "focus" indicates a module
+
+    for (let sub in settings.focus)
+      mappedSettings[sub] = [val, mapVal(sub, settings[sub], val)];
+    return mappedSettings;
+
+  } else if ("ignore" in settings) { //Assumes that the key "ignore" (for attributes) indicates a submodule
+
+    for (let att in settings.ignore)
+      mappedSettings[att] = [val, mapVal(att, settings.ignore[att], val)];
+    return mappedSettings;
+
+  } else if ("focus" in settings[module]) {
+
+    for (let sub in settings[module].focus)
+      mappedSettings[sub] = [val, mapVal(sub, settings[sub], val)];
+    return mappedSettings;
+
+  }
+
+  throw Error("Unexpected construct in module \"" + module + "\"");
+}
+
+//*********************************************************************************
+//Meant for printing out the output given by mapVal
+//*********************************************************************************
+function printMapped(mappedSettings, spaces = 2, adder = 2) {
+  if (mappedSettings === {})
+    return;
+
+  for (let sub in mappedSettings) {
+    let spacing = " ".repeat(spaces);
+    console.log(spacing + sub);
+    printMapped(mappedSettings[sub][1], spaces + adder);
+  }
+}
+
+
+module.exports.parse = parse;
+module.exports.getData = getData;
+module.exports.diagStd = diagStd;
+module.exports.diagRecommended = diagRecommended;
+module.exports.testVideo = testVideo;
+module.exports.mapVal = mapVal;
+module.exports.printMapped = printMapped;
---
+++ project/tests/video.test.js
@@ -0,0 +1,276 @@
+const getData = require("./test-helpers").getData;
+const mapVal = require("./test-helpers").mapVal;
+const testVideo = require("./test-helpers").testVideo;
+
+const gl = require("../common/globals");
+
+
+const TERMINATED = "9BfVn7vEVlM";
+const REMOVED = "HDeadIvpRTw";
+const NORMAL_VIDEO_1 = "BOh5p9i4r8E";
+const NORMAL_VIDEO_2 = "jNQXAC9IVRw"; //Popular!
+const AGE_RESTRICTED_1 = "7m1xFXnuNqA";
+const STREAMED_1 = "1vqkFcZ3xxc";
+const STREAMED_2 = "5yXuWYWBEag"; //Popular!
+const STREAMED_3 = "2RNkRaNfCp4"; //Popular! Chat disabled
+const LIVESTREAM = "jfKfPfyJRdk"; //LIVE! May often change
+
+jest.useRealTimers();
+jest.setTimeout(100000);
+
+
+test("Bad link", async () => {
+  let shellArgs = "video -i bbbbbbbbbbb";
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  expect(savedData).toBe(-1);
+});
+
+test("Channel terminated", async () => {
+  let shellArgs = `video -i ${TERMINATED}`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  expect(savedData).toBe(-1);
+});
+
+test("Video removed by uploader", async () => {
+  let shellArgs = `video -i ${REMOVED}`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  expect(savedData).toBe(-1);
+});
+
+test("Basic scrape 1", async () => {
+  let shellArgs = `video -i ${STREAMED_1}`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  let settings = pack[1];
+
+  let ignore = mapVal("video", settings, false);
+  let focus = mapVal("video", settings, true);
+  let limits = mapVal("video", settings, Number.POSITIVE_INFINITY);
+  let norp = false;
+
+  testVideo(savedData, focus, ignore, limits, norp);
+  expect(savedData).toBeInstanceOf(Object);
+});
+
+test("Basic scrape 2", async () => {
+  let shellArgs = `video -i ${STREAMED_3} comments --lim 10 # recommended --lim 10 # chat --lim 10 #`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  let settings = pack[1];
+
+  let ignore = mapVal("video", settings, false);
+  let focus = mapVal("video", settings, true);
+  let limits = mapVal("video", settings, Number.POSITIVE_INFINITY);
+  let norp = false;
+
+  limits.comments[0] = 10;
+  limits.recommended[0] = 10;
+  limits.chat[0] = 10;
+
+  testVideo(savedData, focus, ignore, limits, norp);
+  expect(savedData).toBeInstanceOf(Object);
+});
+
+test("Ongoing livestream", async () => {
+  let shellArgs = `video -i ${LIVESTREAM} recommended --lim 30`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  let settings = pack[1];
+
+  let ignore = mapVal("video", settings, false);
+  let focus = mapVal("video", settings, true);
+  let limits = mapVal("video", settings, Number.POSITIVE_INFINITY);
+  let norp = false;
+
+  limits.recommended[0] = 30;
+
+  testVideo(savedData, focus, ignore, limits, norp);
+  expect(savedData).toBeInstanceOf(Object);
+});
+
+
+test("Seperate recommended", async () => {
+  let shellArgs = `video -i ${NORMAL_VIDEO_1} recommended --seperate`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  let settings = pack[1];
+
+  let ignore = mapVal("video", settings, false);
+  let focus = mapVal("video", settings, true);
+  let limits = mapVal("video", settings, Number.POSITIVE_INFINITY);
+  let norp = false;
+
+  testVideo(savedData, focus, ignore, limits, norp);
+  expect(savedData).toBeInstanceOf(Object);
+});
+
+test("Focus 1", async () => {
+  let shellArgs = `video -i ${NORMAL_VIDEO_2} --focus meta`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  let settings = pack[1];
+
+  let ignore = mapVal("video", settings, false);
+  let focus = mapVal("video", settings, false);
+  let limits = mapVal("video", settings, Number.POSITIVE_INFINITY);
+  let norp = false;
+
+  focus.meta[0] = true;
+
+  testVideo(savedData, focus, ignore, limits, norp);
+  expect(savedData).toBeInstanceOf(Object);
+});
+
+test("Focus 2", async () => {
+  let shellArgs = `video -i ${AGE_RESTRICTED_1} --exclude meta --exclude chat --exclude recommended`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  let settings = pack[1];
+
+  let ignore = mapVal("video", settings, false);
+  let focus = mapVal("video", settings, true);
+  let limits = mapVal("video", settings, Number.POSITIVE_INFINITY);
+  let norp = false;
+
+  focus.meta[0] = false;
+  focus.chat[0] = false;
+  focus.recommended[0] = false;
+
+  testVideo(savedData, focus, ignore, limits, norp);
+  expect(savedData).toBeInstanceOf(Object);
+});
+
+test("Focus recommended 1", async () => {
+  let shellArgs = `video -i ${STREAMED_2} recommended --focus videos videos --lim 10 # # --focus recommended`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  let settings = pack[1];
+
+  let ignore = mapVal("video", settings, false);
+  let focus = mapVal("video", settings, false);
+  let limits = mapVal("video", settings, Number.POSITIVE_INFINITY);
+  let norp = false;
+
+  focus.recommended[0] = true;
+  focus.recommended[1].videos[0] = true;
+  limits.recommended[1].videos[0] = 10;
+
+  testVideo(savedData, focus, ignore, limits, norp);
+  expect(savedData).toBeInstanceOf(Object);
+});
+
+test("Focus recommended 2", async () => {
+  let shellArgs = `video -i ${STREAMED_3} recommended --lim 4 videos --lim 5 # playlists --lim 1 # --focus videos --focus playlists # --focus recommended`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  let settings = pack[1];
+
+  let ignore = mapVal("video", settings, false);
+  let focus = mapVal("video", settings, false);
+  let limits = mapVal("video", settings, Number.POSITIVE_INFINITY);
+  let norp = false;
+
+  focus.recommended[0] = true;
+  focus.recommended[1].videos[0] = true;
+  focus.recommended[1].playlists[0] = true;
+  limits.recommended[0] = 4;
+  limits.recommended[1].videos[0] = 5;
+  limits.recommended[1].playlists[0] = 1;
+
+  testVideo(savedData, focus, ignore, limits, norp);
+  expect(savedData).toBeInstanceOf(Object);
+});
+
+
+test("Ignore 1", async () => {
+  let shellArgs = `video -i ${NORMAL_VIDEO_1} comments --ignore author --ignore text --ignore id --noreply # --exclude recommended`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  let settings = pack[1];
+
+  let ignore = mapVal("video", settings, false);
+  let focus = mapVal("video", settings, true);
+  let limits = mapVal("video", settings, Number.POSITIVE_INFINITY);
+  let norp = true;
+
+  ignore.comments[1].author[0] = true;
+  ignore.comments[1].text[0] = true;
+  ignore.comments[1].id[0] = true;
+  focus.recommended[0] = false;
+
+  testVideo(savedData, focus, ignore, limits, norp);
+  expect(savedData).toBeInstanceOf(Object);
+});
+
+test("Ignore 2", async () => {
+  let shellArgs = `video -i ${STREAMED_2} comments --lim 25 --ignore picture --ignore text --ignore text --ignore channelId # recommended --lim 25 videos --ignore published # # chat --lim 25 --ignore timestamp #`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  let settings = pack[1];
+
+  let ignore = mapVal("video", settings, false);
+  let focus = mapVal("video", settings, true);
+  let limits = mapVal("video", settings, Number.POSITIVE_INFINITY);
+  let norp = false;
+
+  ignore.comments[1].picture[0] = true;
+  ignore.comments[1].text[0] = true;
+  ignore.comments[1].channelId[0] = true;
+  ignore.recommended[1].videos[1].published[0] = true;
+  ignore.chat[1].timestamp[0] = true;
+  limits.comments[0] = 25;
+  limits.recommended[0] = 25;
+  limits.chat[0] = 25;
+
+  testVideo(savedData, focus, ignore, limits, norp);
+  expect(savedData).toBeInstanceOf(Object);
+});
+
+
+test("Behemoth", async () => {
+  let shellArgs = `video -i ${STREAMED_2} meta # meta --ignore comments --ignore uploader --ignore subscribers --ignore handle --ignore channelId # comments --noreply --lim 46 --ignore picture --ignore text --ignore text --ignore channelId # chat --topchat --lim 50 --ignore channelId --ignore text # recommended --lim 51 videos --ignore published # playlists --ignore firstVideoId #`;
+  let pack = await getData(shellArgs);
+
+  let savedData = pack[0];
+  let settings = pack[1];
+
+  let ignore = mapVal("video", settings, false);
+  let focus = mapVal("video", settings, true);
+  let limits = mapVal("video", settings, Number.POSITIVE_INFINITY);
+  let norp = true;
+
+  ignore.meta[1].comments[0] = true;
+  ignore.meta[1].uploader[0] = true;
+  ignore.meta[1].subscribers[0] = true;
+  ignore.meta[1].handle[0] = true;
+  ignore.meta[1].channelId[0] = true;
+  ignore.comments[1].picture[0] = true;
+  ignore.comments[1].text[0] = true;
+  ignore.comments[1].channelId[0] = true;
+  ignore.recommended[1].videos[1].published[0] = true;
+  ignore.recommended[1].playlists[1].firstVideoId[0] = true;
+  ignore.chat[1].channelId[0] = true;
+  ignore.chat[1].text[0] = true;
+  limits.comments[0] = 46;
+  limits.recommended[0] = 51;
+  limits.chat[0] = 50;
+
+  testVideo(savedData, focus, ignore, limits, norp);
+  expect(savedData).toBeInstanceOf(Object);
+});
